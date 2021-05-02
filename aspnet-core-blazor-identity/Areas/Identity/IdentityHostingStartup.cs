7ae605543a63c1f30eb94294daa5d4d17137b598
keyur patel <kppatel9409402056@gmail.com>
2021-05-02 19:06:23 +0530

    Add project files.

---
+++ aspnet-core-blazor-identity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using aspnet_core_blazor_identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(aspnet_core_blazor_identity.Areas.Identity.IdentityHostingStartup))]
+namespace aspnet_core_blazor_identity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<aspnet_core_blazor_identityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("aspnet_core_blazor_identityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<aspnet_core_blazor_identityContext>();
+            });
+        }
+    }
+}
